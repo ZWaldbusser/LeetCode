87b11f9f74a23cd46217d569ff3519a556b07f66
ZWaldbusser <zwaldbusser@gmail.com>
2024-04-22 15:08:33 -0500

    Create 202. Happy Number.cpp

---
+++ 202. Happy Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        while ((n != 4) && (n != 1)){
+            int sum = 0;
+            while (n > 0){
+                sum += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1;
+    }
+    
+};
