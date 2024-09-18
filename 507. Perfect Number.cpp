d0fee154b2e153e4b7a529afe9f61c5596059f67
ZWaldbusser <103952704+ZWaldbusser@users.noreply.github.com>
2024-09-18 13:15:10 -0500

    Create 507. Perfect Number.cpp

---
+++ 507. Perfect Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+
+    bool checkPerfectNumber(int num) {
+        int temp = 0;
+        for(int i = 1; i <= num/2; i++) {
+            if(num % i == 0) {
+                temp += i;
+            }
+        }
+        return (num==temp) ? true : false;
+    }
+};
