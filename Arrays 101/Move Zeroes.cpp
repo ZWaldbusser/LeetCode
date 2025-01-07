26d89853a330b4582b64c668b8f6c574e3208f06
ZWaldbusser <zwaldbusser@gmail.com>
2025-01-07 12:49:34 -0600

    Create Move Zeroes.cpp

---
+++ Arrays 101/Move Zeroes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        //Bubble sort
+        int temp = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0) {
+                nums[temp] = nums[i];
+                temp++;
+            }
+        }
+        for(int i = temp; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+
+    }
+};
